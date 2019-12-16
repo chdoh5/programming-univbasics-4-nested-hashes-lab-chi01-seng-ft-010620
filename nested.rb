25563dc3b43d51da5297cb7b44f9b1a324b087c4
Christine Doherty <christineadoherty@gmail.com>
2019-12-16 22:32:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -135,7 +135,7 @@ def adding_to_dennis
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages][1] = "Assemby"
+     programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
      
      programmer_hash
 
